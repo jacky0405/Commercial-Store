cfd82c5228388ea64318aff7636156d0c0acb163
jacky <jacky0405@ymail.com>
2022-01-20 12:37:22 +0800

    modufy for Heroku

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const jsonServer = require('json-server')
 const jwt = require('jsonwebtoken')
 const fs = require('fs');
-const express = require('express');
-const server = jsonServer.create();
-const path = require('path');
+const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults({})
+const middlewares = jsonServer.defaults({
+    static: "./build"
+})
 const port = process.env.PORT || 5000;
 server.use(jsonServer.bodyParser)
-server.use('/db', middlewares, router);
-server.use(express.static(path.join(__dirname, 'build')));
-server.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+server.use(middlewares)
 server.use(
     jsonServer.rewriter({
         "/api/*": "/$1"
